655cfc9d069260d1ee09bfd643b317f3390aa735
Leonardo Rossetto <leonardoxh@gmail.com>
2018-04-05 18:22:12 +0200

    Reverting change and improving shared preference name

--- app/src/main/java/com/github/leonardoxh/keystore/CipherPreferencesStorage.java
+++ app/src/main/java/com/github/leonardoxh/keystore/CipherPreferencesStorage.java
@@ -16,38 +16,39 @@
 package com.github.leonardoxh.keystore;
 
 import android.content.Context;
-import android.preference.PreferenceManager;
 import android.util.Base64;
 
 import javax.annotation.Nullable;
 
 final class CipherPreferencesStorage {
+    private static final String SHARED_PREFERENCES_NAME = CipherPreferencesStorage.class.getName() + "_security_storage";
+
     private CipherPreferencesStorage() {
         throw new AssertionError();
     }
 
     private static void saveKeyString(Context context, String alias, String value) {
-        PreferenceManager.getDefaultSharedPreferences(context)
+        context.getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
                 .edit()
                 .putString(alias, value)
                 .apply();
     }
 
     static void remove(Context context, String alias) {
-        PreferenceManager.getDefaultSharedPreferences(context)
+        context.getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
                 .edit()
                 .remove(alias)
                 .apply();
     }
 
     static boolean containsAlias(Context context, String alias) {
-        return PreferenceManager.getDefaultSharedPreferences(context)
+        return context.getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
                 .contains(alias);
     }
 
     @Nullable
     private static String getKeyString(Context context, String alias) {
-        return PreferenceManager.getDefaultSharedPreferences(context)
+        return context.getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
                 .getString(alias, null);
     }
 
