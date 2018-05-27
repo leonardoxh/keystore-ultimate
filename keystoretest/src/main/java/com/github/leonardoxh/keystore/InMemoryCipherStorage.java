35f6c5940584586131e15bb27bf5db31ded4fa88
Leonardo Rossetto <leonardoxh@gmail.com>
2018-05-27 14:37:42 +0200

    Moving packages arround

--- keystoretest/src/main/java/com/github/leonardoxh/keystore/InMemoryCipherStorage.java
+++ keystoretest/src/main/java/com/github/leonardoxh/keystore/InMemoryCipherStorage.java
@@ -1,7 +1,24 @@
+/*
+ * Copyright 2018 Leonardo Rossetto
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.leonardoxh.keystore;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.annotation.Nullable;
 
 public class InMemoryCipherStorage implements CipherStorage {
     private final Map<String, String> values = new HashMap<>();
@@ -11,6 +28,7 @@ public class InMemoryCipherStorage implements CipherStorage {
         values.put(alias, value);
     }
 
+    @Nullable
     @Override
     public String decrypt(String alias) {
         return values.get(alias);
@@ -18,7 +36,7 @@ public class InMemoryCipherStorage implements CipherStorage {
 
     @Override
     public boolean containsAlias(String alias) {
-        return values.containsValue(alias);
+        return values.containsKey(alias);
     }
 
     @Override
---
+++ keystoretest/src/test/java/com/github/leonardoxh/keystore/InMemoryCipherStorageTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2018 Leonardo Rossetto
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.leonardoxh.keystore;
+
+import org.junit.Test;
+
+import static com.google.common.truth.Truth.assertThat;
+
+public class InMemoryCipherStorageTest {
+    private final CipherStorage subject = new InMemoryCipherStorage();
+
+    @Test
+    public void encryptValues() {
+        subject.encrypt("test", "test-value");
+        assertThat(subject.containsAlias("test")).isTrue();
+    }
+
+    @Test
+    public void decryptValues() {
+        subject.encrypt("test", "test-value");
+        assertThat(subject.decrypt("test")).isEqualTo("test-value");
+    }
+
+    @Test
+    public void removeValues() {
+        subject.encrypt("test", "test-alias");
+        subject.removeKey("test");
+        assertThat(subject.containsAlias("test")).isFalse();
+    }
+
+    @Test
+    public void replaceValues() {
+        subject.encrypt("test", "test-alias");
+        subject.saveOrReplace("test", "test-value-replaced");
+        assertThat(subject.decrypt("test")).isEqualTo("test-value-replaced");
+    }
+}
